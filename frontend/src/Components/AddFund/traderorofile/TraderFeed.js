808f56528fb5175df6829ef9ee91bca064d850e9
Sanu Das <sanukr1202@gmail.com>
2023-04-25 05:07:07 +0530

    done dana done

--- frontend/src/Components/AddFund/traderorofile/TraderFeed.js
+++ frontend/src/Components/AddFund/traderorofile/TraderFeed.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect,useRef } from 'react'
 import styles from './TraderFeed.module.css'
 import Post from '../../Discover/Post'
 import Copiers from '../../Discover/Copiers'
@@ -8,117 +8,140 @@ import Traders from '../../Discover/Traders'
 import { TextField } from '@mui/material'
 import axios from 'axios'
 import Navbar from '../../Navbar/Navbar'
+import { Audio ,ColorRing} from "react-loader-spinner";
 // import Navbar from '../Navbar/Navbar'
 // import { TextField } from 'react-admin'
 
 function TraderFeed (props) {
-  const { sideBarShow ,items} = props
-
-//   console.log(sideBarShow,"sideBarShow");
-  const [newPost, setnewPost] = useState('')
-  const [flag, setflag] = useState(false)
+  const {sideBarShow,items}= props
+  const fileRef = useRef(null);
+  const [newPost, setnewPost] = useState("");
+  const [flag, setflag] = useState(false);
+  const [deleteflag, setdeleteflag] = useState(false);
+  const [loader, setloader] = useState(true);
   const handlePost = (e) => {
-    setnewPost(e.target.value)
+    setnewPost(e.target.value);
     // e.target.value = "";
     // console.log(newPost);
-  }
+  };
+  let [loadingState, setLoadingState] = useState(false);
   const handlePostSubmit = async (e) => {
-    e.preventDefault()
-    const formdata = new FormData()
-    formdata.append('postText', newPost)
-    formdata.append('postImage', postImage)
-    formdata.append('postImageName', postImageName)
-    // console.log(postImage)
-    // console.log(formdata)
-    setflag(true)
+    e.preventDefault();
+    setLoadingState(true);
+    const formdata = new FormData();
+    formdata.append("postText", newPost);
+    // formdata.append("postImage", postImage);
+    formdata.append("file", fileRef.current.files[0]);
+    formdata.append("postImageName", postImageName);
+    console.log(postImage);
+    console.log(formdata);
+
+    setloader(true);
+    setTimeout(() => {
+      console.log("20000000000000000");
+      setflag(true);
+      setloader(false);
+    }, 4000);
+    // if(flag===false){
+
+    // }
+
+    // setflag(true);
+    console.log("flag is ", flag);
     const pst = await axios.post(
-      'http://localhost:8081/api/users/uploadPost',
+      "http://localhost:8081/api/users/uploadPost",
       // {
       // postText: newPost
       formdata
       // }
-    )
+    );
+
     if (pst.length === 0) {
-      console.log('Post failed!')
+      console.log("Post failed!");
       // return 0;
     } else {
-      console.log('Post successful!')
+      console.log("Post successful!");
       // const history =useHistory();
-      // setLoadingState(false)
+      setLoadingState(false);
       // Navigate("/discover")
     }
-  }
-  const [postdata, setPostdata] = useState('')
+  };
+  const [postdata, setPostdata] = useState("");
+
   const handleGetPost = async () => {
-    const pst = await axios.get(
-      'http://localhost:8081/api/users/getPost')
-    // console.log('sdf')
-    // console.log(pst)
+    const pst = await axios.get("http://localhost:8081/api/users/getPost");
+    console.log("sdf");
+    console.log(pst);
     if (pst.length === 0) {
-      console.log('No post!')
+      console.log("No post!");
       // return 0;
     } else {
-      console.log('Post fetched successful!')
+      console.log("Post fetched successful!");
       // const history =useHistory();
       // setLoadingState(false);
       // Navigate("/discover")
     }
-    setflag(false)
-    setPostdata(pst.data.result)
+    setflag(false);
+    setloader(false);
+    setPostdata(pst.data.result);
 
-    setnewPost('')
-    setpostImageName('')
-    setpostImage('')
-  }
+    setnewPost("");
+    setpostImageName("");
+    setpostImage("");
+  };
 
   useEffect(() => {
     // let pp;
-    handleGetPost()
+    handleGetPost();
+    console.log("flag issssssss", flag);
+    setloader(false);
     // setflag(false);
-  }, [flag])
-//   console.log({ postdata })
+  }, [flag]);
+  console.log({ postdata: postdata });
+
+  useEffect(() => {
+    setLoadingState(false);
+  }, [postdata]);
 
   const calculateTime = (createdAt) => {
-    const createdAtDate = new Date(createdAt)
-    const currentDate = new Date()
+    const createdAtDate = new Date(createdAt);
+    const currentDate = new Date();
 
-    const timeDiff = Math.abs(currentDate.getTime() - createdAtDate.getTime()) // in milliseconds
+    const timeDiff = Math.abs(currentDate.getTime() - createdAtDate.getTime()); // in milliseconds
 
-    const seconds = Math.floor(timeDiff / 1000)
-    const minutes = Math.floor(seconds / 60)
-    const hours = Math.floor(minutes / 60)
-
-    if (minutes === 0 && hours === 0) {
-      return `${seconds} seconds ago`
-    } else if (hours === 0) {
-      return `${minutes} minutes ago`
+    const seconds = Math.floor(timeDiff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    let str;
+    if (minutes == 0 && hours == 0) {
+      return `${seconds} seconds ago`;
+    } else if (hours == 0) {
+      return `${minutes} minutes ago`;
     } else {
-      return `${hours} hours ago`
+      return `${hours} hours ago`;
     }
     // return `created ${hours} hours, ${minutes % 60} minutes, ${seconds % 60} seconds`
-  }
-  const [postImage, setpostImage] = useState('')
-  const [postImageName, setpostImageName] = useState('')
+  };
+  const [postImage, setpostImage] = useState("");
+  const [postImageName, setpostImageName] = useState("");
   const handleImageChange = (e) => {
-    setpostImage(e.target.files[0])
-    setpostImageName(e.target.files[0].name)
-  }
-
-  // const [loadingState, setLoadingState] = useState(false)
+    setpostImage(e.target.files[0]);
+    setpostImageName(e.target.files[0].name);
+  };
   return (
     <>
     <Navbar/>
       <div className={styles.feedOuterContainer}>
-        <div className={styles.feedInner}>
-          <div className={styles.post}>
-            <div className={styles.postTop}>
-              <div className={styles.dp}>
+       <div className={styles["feedInner"]}>
+          <div className={styles["post"]}>
+            <div className={styles["postTop"]}>
+              <div className={styles["dp"]}>
                 <img
                   src={require('../../../Assets/images//Navbar/defaultDP.png')}
                   alt=""
                 />
               </div>
-              <div className={styles.textAr}>
+              <div className={styles["textAr"]}>
                 <form action="">
                   {/* <input
                     size="50"
@@ -127,20 +150,28 @@ function TraderFeed (props) {
                     placeholder="What's on your mind"
                     onChange={handlePost}
                   /> */}
-                  <TextField id="standard-basic" label="What's on your mind" variant="standard" name="post_text" onChange={handlePost} style={{
-                    // width: '19em',
-                    // position: 'relative',
-                    // right: '2em',
-                    // height: '27px',
-                    // fontSize: '1.2rem'
-                  }}/>
+                  <TextField
+                    id="standard-basic"
+                    label="What's on your mind"
+                    variant="standard"
+                    name="post_text"
+                    onChange={handlePost}
+                    style={
+                      {
+                        // width: '19em',
+                        // position: 'relative',
+                        // right: '2em',
+                        // height: '27px',
+                        // fontSize: '1.2rem'
+                      }
+                    } 
+                  />
                   <br />
                   <input
-                  className={styles.fileInp}
                     type="file"
                     name="myImage"
+                    ref={fileRef}
                     onChange={handleImageChange}
-                    style={{ width: '15em', marginTop: '0.6vw', marginBottom: '1vw' }}
                   />
                   <br />
                   <div className={styles.postBtn}>
@@ -149,22 +180,57 @@ function TraderFeed (props) {
                 </form>
               </div>
             </div>
-
-            {/* <div className={styles['post-bottom']}>
-              <div className={styles.action}>
-                <i className={'fa fa-image'}></i>
-                <span>Photo/Video</span>
-              </div>
-            </div> */}
           </div>
-          <div className={styles.recentPosts} >
-          {postdata?.length > 0 &&
-                  postdata.map((item, index) => {
-                    // console.log('index is', index)
-                    return (
-                      <Post key={item.id} postCreatorName = {items.name} timeOfPost={calculateTime(item.createdAt)} postText={item.postText} postImage ={item.postImage}/>
-                    )
-                  })}
+          {loadingState && (
+            <div
+              style={{
+                width: "200px",
+                height: "44px",
+                position: "relative",
+                left: "22em",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-start",
+                top: "1em"
+                // backgroundColor: "black",
+              }}
+            >
+              {/* <Audio
+                height="80"
+                width="80"
+                radius="9"
+                color="green"
+                ariaLabel="loading"
+                wrapperStyle
+                wrapperClass
+              /> */}
+              <ColorRing
+                visible={true}
+                height="50"
+                width="50"
+                ariaLabel="blocks-loading"
+                wrapperStyle={{}}
+                wrapperClass="blocks-wrapper"
+                colors={["#0095eb", "#0095eb", "#0095eb", "#abbd81", "#0095eb"]}
+              /> Loading...
+            </div>
+          )}
+          <div className={styles["recentPosts"]} style={{ color: "black" }}>
+            {console.log("fsdddddddddd", postdata.length)}
+            {postdata?.length > 0 &&
+              postdata.map((item, index) => {
+                console.log("index is", index);
+                return (
+                  <Post
+                    postCreatorName={items.name}
+                    timeOfPost={calculateTime(item.createdAt)}
+                    postText={item.postText}
+                    postImage={item.postImage}
+                    postId={item._id}
+                    setdelflag={setflag}
+                  />
+                );
+              })}
             {/* <Post postCreatorName = "Ayush Raj" timeOfPost="5 hrs ago" postText="Bitcoin’s stealth rally erases its losses for the year Bitcoin’s stealth rally erases its losses for the year Bitcoin’s stealth rally erases its losses for the year  " postImage = "platinumBg.jpg"/>
             <Post postCreatorName = "Ayush Dingla" timeOfPost="4 hrs ago" postText="Bitcoin’s stealth rally erases its losses for the year Bitcoin’s stealth rally erases its losses for the year Bitcoin’s stealth rally erases its losses for the year  " postImage = "goldBg.jpg"/>
             <Post postCreatorName = "Praveen Raj" timeOfPost="10 hrs ago" postText="Bitcoin’s stealth rally erases its losses for the year Bitcoin’s stealttyfyf fhjgjd hgdhw wge hevhr the year  " postImage = "platinumBg.jpg"/>
